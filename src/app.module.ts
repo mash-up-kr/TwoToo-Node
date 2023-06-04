9add0f85a430371271174c57eb0012bcbe43378c
seohyun0120 <sarahseohyun@gmail.com>
2023-06-04 19:12:44 +0900

    feat: Add Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
@@ -9,8 +11,22 @@ import { ChallengeController } from './challenge/challenge.controller';
 import { ChallengeService } from './challenge/challenge.service';
 
 @Module({
-  imports: [UserModule, ChallengeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+        useNewUrlParser: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule, ChallengeModule
+  ],
   controllers: [AppController, UserController, ChallengeController],
   providers: [AppService, UserService, ChallengeService],
 })
+
 export class AppModule { }

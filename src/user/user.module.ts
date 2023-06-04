33d806a649ac138acbebf7c845934a90e1f54762
seohyun0120 <sarahseohyun@gmail.com>
2023-06-04 20:58:20 +0900

    feat: mongoose 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   providers: [UserService],
   controllers: [UserController]
 })

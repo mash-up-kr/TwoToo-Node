fb36aafbe93e48c25774b23afca4a95eda50ec73
seohyun0120 <sarahseohyun@gmail.com>
2023-06-04 20:59:11 +0900

    feat: userService 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schema/user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>
+  ) { }
+
+  async createUser() {
+    const userNo = 0; // auto-increment 적용?
+    const user = await this.userModel.create({ userNo });
+
+    await user.save();
+    return user;
+  }
+
+  async updateUser({ userNo, nickname, partnerNo }) {
+    const user = await this.userModel.findOne({ userNo });
+
+    if (!user) {
+      throw new Error('Not Found User');
+    }
+
+    if (user.partnerNo) {
+      throw new Error('Partner already exists.');
+    }
+
+    user.nickname = nickname;
+    user.partnerNo = partnerNo;
+
+    await user.save();
+    return user;
+  }
+
+  async me(userNo: number) {
+    const user = await this.userModel.findOne({ userNo });
+    if (!user) {
+      throw new Error('Not Found User');
+    }
+
+    return user;
+  }
+}

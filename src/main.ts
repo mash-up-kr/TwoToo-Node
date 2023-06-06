cc53d494ae2ef4294ccf78543c2063f5bca4d573
seohyun0120 <sarahseohyun@gmail.com>
2023-06-06 23:55:12 +0900

    feat: Swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
   await app.listen(3000);
 }
+
 bootstrap();
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,19 +1,17 @@
-import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const options = new DocumentBuilder()
-    .setTitle('TwoToo Node')
-    .setDescription('Two Too Node API')
-    .setVersion('1.0')
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('TwoToo(투투) - API')
+    .setDescription('TwoToo 서비스')
+    .setVersion('1.0.0')
+    .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3001);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    useGlobalPrefix: true,
+    swaggerOptions: { redirect: false },
+  });
 }
-bootstrap();

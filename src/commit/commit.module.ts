51f6906191d2f2b3d1ff9692e88cdabe6b8c539c
seohyun0120 <sarahseohyun@gmail.com>
2023-06-17 23:25:49 +0900

    feat: Commit에서 Challenge를 사용할 수 있도록 추가

--- src/commit/commit.module.ts
+++ src/commit/commit.module.ts
@@ -4,11 +4,13 @@ import { CommitController } from './commit.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Commit, CommitSchema } from './schema/commit.schema';
 import { CommitCounter, CommitCounterSchema } from './schema/commit-counter.schema';
+import { Challenge, ChallengeSchema } from 'src/challenge/schema/challenge.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Commit.name, schema: CommitSchema },
+      { name: Challenge.name, schema: ChallengeSchema },
       { name: CommitCounter.name, schema: CommitCounterSchema },
     ]),
   ],

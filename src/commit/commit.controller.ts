19c33b5db88e5b524300946b20dd32b165703c18
seohyun0120 <sarahseohyun@gmail.com>
2023-06-17 22:40:04 +0900

    feat: commit 컨트롤러 설명 수정

--- src/commit/commit.controller.ts
+++ src/commit/commit.controller.ts
@@ -8,8 +8,9 @@ import { CommitService } from './commit.service';
 export class CommitController {
   constructor(private readonly commit: CommitService) { }
 
+  // @TODO AuthGaurd
   @Post('/commit')
-  @ApiOperation({ description: '인증을 진행합니다.' })
+  @ApiOperation({ description: '챌린지 인증을 진행합니다.' })
   async createCommit(@Body() commitPayload: any): Promise<CommitResponse> {
     const commit = await this.commit.createCommit(commitPayload);
 
@@ -18,7 +19,7 @@ export class CommitController {
 
   // @TODO AuthGaurd
   @Get('/commit/:commitNo')
-  @ApiOperation({ description: '인증 정보를 조회합니다.' })
+  @ApiOperation({ description: '챌린지 인증 정보를 조회합니다.' })
   async getCommit(
     @Req() req: any,
     @Param('commitNo') commitNo: string
@@ -28,14 +29,15 @@ export class CommitController {
     return commit;
   }
 
+  // @TODO AuthGaurd
   @Post('/commit/:commitNo/comment')
-  @ApiOperation({ description: '파트너의 인증에 칭찬 댓글을 추가합니다.' })
+  @ApiOperation({ description: '파트너의 챌린지 인증에 칭찬 댓글을 추가합니다.' })
   async createComment(
     @Req() req: any,
     @Body() { partnerComment }: CommitCommentDto,
     @Param('commitNo') commitNo: string,
   ): Promise<CommitResponse> {
-    const commit = await this.commit.updateCommit({ commitNo, partnerComment });
+    const commit = await this.commit.updateCommit({ commitNo: parseInt(commitNo), partnerComment });
 
     return commit;
   }

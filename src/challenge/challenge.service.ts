21ac1486eb0186f4697bd2c64de735d09dd270ba
LouiMinister <louiminister@gmail.com>
2023-06-22 16:01:54 +0900

    fix: challenge 생성 service 로직 수정
    - auto increasement
    - 예외처리

--- src/challenge/challenge.service.ts
+++ src/challenge/challenge.service.ts
@@ -6,6 +6,7 @@ import { CreateChallengeDto } from './dto/create-challenge.dto';
 import { UserService } from '../user/user.service';
 import { add } from 'date-fns';
 import { TWOTWO } from '../constants/number';
+import { ChallengeCounter, ChallengeCounterDocument } from './schema/challenge-counter.schema';
 
 @Injectable()
 export class ChallengeService {
@@ -13,14 +14,20 @@ export class ChallengeService {
     private readonly userSvc: UserService,
     @InjectModel(Challenge.name)
     private readonly challengeModel: Model<ChallengeDocument>,
+    @InjectModel(ChallengeCounter.name)
+    private readonly challengeCounterModel: Model<ChallengeCounterDocument>,
   ) {}
 
   async createChallenge(createChallengeDto: CreateChallengeDto): Promise<Challenge> {
     const user1 = await this.userSvc.getUser(createChallengeDto.user1No);
+    // TODO: Validate 로직 깔끔하게 하기
+    if (user1.partnerNo === undefined) throw new Error('파트너가 존재하지 않습니다');
     const user2 = await this.userSvc.getUser(user1.partnerNo);
     const endDate: Date = add(createChallengeDto.startDate, { days: TWOTWO });
 
+    const challengeNo = await this.autoIncrement();
     const challenge = await this.challengeModel.create({
+      challengeNo,
       name: createChallengeDto.name,
       user1: user1,
       user2: user2,
@@ -31,5 +38,19 @@ export class ChallengeService {
 
     await challenge.save();
     return challenge;
+  }
+
+  private async autoIncrement() {
+    let result: { count: number } | null = null;
+
+    while (result === null) {
+      result = await this.challengeCounterModel.findOneAndUpdate(
+        { key: 'challengeNo' },
+        { $inc: { count: 1 } },
+        { upsert: true, returnOriginal: false },
+      );
+    }
+
+    return result!.count;
   }
 }

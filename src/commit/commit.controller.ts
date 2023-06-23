469787a24f10db7274c682e890fa17eee7120332
seohyun0120 <sarahseohyun@gmail.com>
2023-06-23 18:36:12 +0900

    AuthGuard 추가, uri 수정

--- src/commit/commit.controller.ts
+++ src/commit/commit.controller.ts
@@ -1,40 +1,41 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CommitCommentPayload, CommitCreatePayload, CommitResponse } from './dto/commit.dto';
 import { CommitService } from './commit.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @ApiTags('commit')
 @Controller('commit')
 export class CommitController {
-  constructor(private readonly commit: CommitService) {}
+  constructor(private readonly commitSvc: CommitService) { }
 
-  // @TODO AuthGaurd
-  @Post('/commit')
+  @UseGuards(AuthGuard)
+  @Post('')
   @ApiOperation({ description: '챌린지 인증을 진행합니다.' })
   async createCommit(@Req() req: any, @Body() data: CommitCreatePayload): Promise<CommitResponse> {
-    const commit = await this.commit.createCommit({ userNo: parseInt(req.user.userNo), data });
+    const commit = await this.commitSvc.createCommit({ userNo: req.user.userNo, data });
 
     return commit;
   }
 
-  // @TODO AuthGaurd
-  @Get('/commit/:commitNo')
+  @UseGuards(AuthGuard)
+  @Get('/:commitNo')
   @ApiOperation({ description: '챌린지 인증 정보를 조회합니다.' })
   async getCommit(@Req() req: any, @Param('commitNo') commitNo: string): Promise<CommitResponse> {
-    const commit = await this.commit.getCommit(parseInt(commitNo));
+    const commit = await this.commitSvc.getCommit(parseInt(commitNo));
 
     return commit;
   }
 
-  // @TODO AuthGaurd
-  @Post('/commit/:commitNo/comment')
+  @UseGuards(AuthGuard)
+  @Post('/:commitNo/comment')
   @ApiOperation({ description: '파트너의 챌린지 인증에 칭찬 댓글을 추가합니다.' })
   async createComment(
     @Req() req: any,
     @Body() { partnerComment }: CommitCommentPayload,
     @Param('commitNo') commitNo: string,
   ): Promise<CommitResponse> {
-    const commit = await this.commit.updateCommit({ commitNo: parseInt(commitNo), partnerComment });
+    const commit = await this.commitSvc.updateCommit({ commitNo: parseInt(commitNo), partnerComment });
 
     return commit;
   }
--- src/commit/commit.module.ts
+++ src/commit/commit.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CommitService } from './commit.service';
 import { CommitController } from './commit.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { Commit, CommitSchema } from './schema/commit.schema';
 import { CommitCounter, CommitCounterSchema } from './schema/commit-counter.schema';
-import { Challenge, ChallengeSchema } from 'src/challenge/schema/challenge.schema';
+import { Challenge, ChallengeSchema } from '../challenge/schema/challenge.schema';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   imports: [
@@ -14,7 +17,7 @@ import { Challenge, ChallengeSchema } from 'src/challenge/schema/challenge.schem
       { name: CommitCounter.name, schema: CommitCounterSchema },
     ]),
   ],
-  providers: [CommitService],
+  providers: [CommitService, AuthGuard, JwtService],
   controllers: [CommitController],
 })
 export class CommitModule {}

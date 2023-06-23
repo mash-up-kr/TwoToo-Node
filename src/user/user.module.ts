703da3a9e4b76a0fdca3ff752c9cd4ac8f6a41bf
LouiMinister <louiminister@gmail.com>
2023-06-23 17:25:38 +0900

    fix: UserModule 에서 AuthGaurd, JwtService export 하도록 변경

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,7 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
-import { JwtService,JwtModule } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { UserCounter, UserCounterSchema } from './schema/user-counter.schema';
 import { AuthGuard } from 'src/auth/auth.guard';
 
@@ -16,6 +16,6 @@ import { AuthGuard } from 'src/auth/auth.guard';
   ],
   providers: [UserService, AuthGuard, JwtService],
   controllers: [UserController],
-  exports: [UserService],
+  exports: [UserService, AuthGuard, JwtService],
 })
 export class UserModule {}

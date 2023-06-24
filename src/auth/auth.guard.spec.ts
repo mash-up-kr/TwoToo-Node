cc71837a0f1f7eea6b1e18fb2dbd692ac309e19c
seohyun0120 <sarahseohyun@gmail.com>
2023-06-24 23:43:58 +0900

    fix: 테스트 코드 수정

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,7 +1,3 @@
 import { AuthGuard } from './auth.guard';
 
-describe('AuthGuard', () => {
-  it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
-  });
-});
+describe('AuthGuard', () => {});

9f6984479363f83ce854e20d96710c91f8cb3107
LouiMinister <louiminister@gmail.com>
2023-06-28 19:49:35 +0900

    feat: HomeViewResDto 구현

--- src/view/dto/home-view.res.dto.ts
+++ src/view/dto/home-view.res.dto.ts
@@ -1,13 +1,11 @@
 import { Commit } from '../../commit/schema/commit.schema';
-import { User } from '../../user/schema/user.schema';
 import { Challenge } from '../../challenge/schema/challenge.schema';
+import { HomeViewStateType } from '../view.type';
 
 export class HomeViewResDto {
-  viewState: string;
-  user1: User; // 챌린지 생성자
-  user2: User; // 챌린지 수락자
+  viewState: HomeViewStateType;
   challengeTotal: number; // 전체 진행한 챌린지 횟수
-  challenge: Challenge | null; // 현재 진행중인 챌린지 정보
+  onGoingChallenge: Challenge | null; // 현재 진행중인 챌린지 정보
   user1Commit: Commit | null; // 오늘 인증 정보
   user2Commit: Commit | null;
 }

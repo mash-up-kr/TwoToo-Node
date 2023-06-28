234f68f36e358a4d858081e3a0a9385100d67ba6
LouiMinister <louiminister@gmail.com>
2023-06-28 19:58:09 +0900

    feat: ViewController에 view/home 엔드포인트 구현

--- src/view/view.controller.ts
+++ src/view/view.controller.ts
@@ -5,6 +5,9 @@ import { ChallengeService } from '../challenge/challenge.service';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/auth.guard';
 import { HomeViewResDto } from './dto/home-view.res.dto';
+import { HomeViewService } from './homeView.service';
+import { JwtParam } from '../auth/auth.user.decorator';
+import { JwtPayload } from '../auth/auth.types';
 
 @Controller('view')
 export class ViewController {
@@ -12,13 +15,14 @@ export class ViewController {
     private readonly challengeSvc: ChallengeService,
     private readonly userSvc: UserService,
     private readonly commitSvc: CommitService,
+    private readonly homeViewSvc: HomeViewService,
   ) {}
 
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Get('home')
   @ApiOperation({ description: '홈 화면을 조회합니다.' })
-  async getHomeView(): Promise<HomeViewResDto> {
-    return new HomeViewResDto();
+  async getHomeView(@JwtParam() jwtParam: JwtPayload): Promise<HomeViewResDto> {
+    return await this.homeViewSvc.createHomeViewResponse(jwtParam.userNo);
   }
 }
